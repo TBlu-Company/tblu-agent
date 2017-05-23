b6bd0af03b60f21303e001088caae3fa49a0e7ea
Felipe A Soares <felipe.soares@paxtecnologia.com.br>
2017-05-23 16:00:08 -0300

    Help.js

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+const help = require('./lib/helper/help');
+
+function atividade() {
+  if (process.argv.length < 3) {
+    console.log('start');
+  } else {
+    switch (process.argv[2]) {
+      case 'start':
+        console.log('start');
+        break;
+      case 'configure':
+        console.log('configure');
+        break;
+      case 'service':
+        console.log('service');
+        break;
+      case 'help':
+        help();
+        break;
+      default:
+        help();
+    };
+  };
+};
+
+atividade();
