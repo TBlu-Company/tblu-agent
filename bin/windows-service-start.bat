7c5f216105f274adad87ad82a2fddf62d2bfc5b3
pauloferreira25 <paulo.ferreira25@gmail.com>
2017-05-23 12:08:35 -0300

    Init V2

--- bin/windows-service-start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %~dp0
-node %~dp0\..\src\services\windows\service-start.js
-ping 127.0.0.1 -n 2 >NUL 2>&1
--- bin/windows-service-uninstall.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-node %~dp0\..\src\services\windows\service-uninstall.js
-ping 127.0.0.1 -n 2 >NUL 2>&1
