c1b4537c4279ecdc58b76baf12ea7bf61b2cb6e2
pauloferreira25 <paulo.ferreira25@gmail.com>
2017-05-24 11:59:17 -0300

    mkdirp

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const path = require('path');
 global.properties = require('./lib/global/properties');
 global.properties.setAppDir(path.dirname(__filename));
-const help = require('./lib/helper/help');
-const appConfigure = require('./lib/orchestration/configure');
-const appService = require('./lib/orchestration/service');
-const appStart = require('./lib/orchestration/start');
 const npmInstallAll = require('./lib/npm/npm-installAll')
 
 const start = () => {
   npmInstallAll().then(() => {
+    const help = require('./lib/helper/help');
+    const appConfigure = require('./lib/orchestration/configure');
+    const appService = require('./lib/orchestration/service');
+    const appStart = require('./lib/orchestration/start');
     if (process.argv.length < 3) {
       appStart();
     } else {
